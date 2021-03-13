cc550ea3ef3b500ed76f610b5100626854ac62f3
nikhuff <nik.huff@me.com>
2021-03-13 03:58:38 -0700

    Adjusted default scene size

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("MainForm.fxml"));
         primaryStage.setTitle("Inventory Management");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 788, 353));
         primaryStage.show();
     }
 
