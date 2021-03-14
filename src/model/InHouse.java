d3a57354bc351560662b36469470f81e3a766c88
nikhuff <nik.huff@me.com>
2021-03-14 00:01:12 -0700

    make constructor public

--- src/model/InHouse.java
+++ src/model/InHouse.java
@@ -1,7 +1,7 @@
 package model;
 
 public class InHouse extends Part {
-    InHouse(int id, String name, double price, int stock, int min, int max, int machineId) {
+    public InHouse(int id, String name, double price, int stock, int min, int max, int machineId) {
         super(id, name, price, stock, min, max);
         this.machineId = machineId;
     }
